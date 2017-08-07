5248effd4aaf023f08377a30a0bd1c9b4df34757
Hyia <azureciella@gmail.com>
2017-08-07 16:08:10 +0900

    modify logName making logic changed

--- gameproject/src/main/java/practice/webgameproject/strategy/engine/child/BattleLogMaker.java
+++ gameproject/src/main/java/practice/webgameproject/strategy/engine/child/BattleLogMaker.java
@@ -35,19 +35,21 @@ public class BattleLogMaker {
 	
 	List<Round> round;
 	
-	public BattleLogMaker(List<ModelHeroTable> attacker, List<Army> attackerArmy,
-			List<ModelHeroTable> defender, List<Army> defenderArmy) {
+	public BattleLogMaker(String attacker_ID,List<ModelHeroTable> attacker, List<Army> attackerArmy,
+			String defender_ID,List<ModelHeroTable> defender, List<Army> defenderArmy) {
 		serialNumber++;
 		logID = serialNumber;
 		logDate = new Date();
-		logName = logDate.toString()+"-"+logID;
 		this.attacker = attacker;
 		this.attackerArmy = attackerArmy;
 		this.defender = defender;
 		this.defenderArmy = defenderArmy;
-		
+		this.attacker_ID = attacker_ID;
+		this.defender_ID = defender_ID;
 		round = new ArrayList<Round>();
 		currentRound = 0;
+		
+		logName = null;
 	}
 
 	public Date getLogDate() {
@@ -59,9 +61,9 @@ public class BattleLogMaker {
 	public BattleLogMaker() {
 		serialNumber++;
 		logID = serialNumber;
-		logName = (new Date()).toString()+"-"+logID;
 		round = new ArrayList<Round>();
 		currentRound = 0;
+		logName = null;
 	}
 
 	
@@ -144,6 +146,9 @@ public class BattleLogMaker {
 		return logID;
 	}
 	public String getLogName() {
+		if(logName == null){
+			return "ERROR_call_getLogName_before_write";
+		}
 		return logName;
 	}
 
@@ -163,6 +168,7 @@ public class BattleLogMaker {
 	}
 
 	public void writeLog() {
+		logName = logDate.toString()+"-"+attacker_ID+"-"+defender_ID;
 		BufferedWriter out = null;
 	    try {
 	        out = new BufferedWriter(new FileWriter(LOGFILEROOT+"/"+logName+".log"));
